2c3b1c1c68d634013a4f9ca6a3f097dd0581a60c
tecnoDevs <carponal@hotmail.com>
2025-11-27 21:39:57 -0500

    App para publicacion

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import "./app.css";
+
+function App() {
+  const [logrado, setLogrado] = useState(true);
+
+  return (
+    <>
+      <main className="main">
+        <section className="main__section">
+          <img src="/public/illustration-article.svg" alt="img_blog" />
+        </section>
+        <section className="main__section--body">
+          <a href="#" onClick={() => setLogrado(!logrado)}>
+            Learning
+          </a>
+          <p>Published 21 Dec 2023</p>
+          <h2>HTML & CSS foundations</h2>
+          <p className="main__section--body--text">
+            These languages are the backbone of every website, defining
+            structure, content, and presentation.
+          </p>
+          <div className="main__footer">
+            <img src="/public/image-avatar.webp" alt="avatar" />
+            <p>Greg Hooper</p>
+          </div>
+        </section>
+        <div className={`modal ${logrado ? "modal__oculto" : ""}`}>
+          <p>Reto Logrado</p>
+        </div>
+      </main>
+      <div className="attribution">
+        Challenge by{" "}
+        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+          Frontend Mentor
+        </a>
+        . Coded by <a href="#">Carlos Alberto Martinez Florez</a>.
+      </div>
+    </>
+  );
+}
+
+export default App;
